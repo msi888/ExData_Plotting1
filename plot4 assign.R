bab3b24f267cc1af78d65af1a1c9b9284f60c396
MARIO SIROCCHI <msirocc2@gmail.com>
2016-01-08 16:51:45 +0100

    message

---
+++ plot4 assign.R
@@ -0,0 +1,23 @@
+setwd("F:/JHU/3_EXPLORATORY DATA")
+getwd()
+rm(list=ls())
+file<- read.table("household_power_consumption.txt", sep=";", header=TRUE, quote= "", na.strings= "?")
+fd<- subset(file, (file$Date == "1/2/2007" | file$Date== "2/2/2007")) 
+
+fd$Date <- as.Date(fd$Date, format = "%d/%m/%Y")
+fd$DT <- as.POSIXct(paste(fd$Date, fd$Time))
+head(fd)
+tail(fd)
+## PLOT4 CREATION
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(fd, {
+        plot(Global_active_power~DT, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+        plot(Voltage~DT, type="l", ylab="Voltage (volt)", xlab="")
+        plot(Sub_metering_1~DT, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+        lines(Sub_metering_2~DT,col='Red')
+        lines(Sub_metering_3~DT,col='Blue')
+        legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+        plot(Global_reactive_power~DT, type="l", ylab="Global Rective Power (kilowatts)",xlab="")
+})
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
